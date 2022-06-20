d988f93fc8b2143744af72832737e03fe372efe1
Christian Araque <christaraque@hotmail.com>
2022-06-19 23:30:30 -0400

    feat(serverPort): changed server port variable

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.SERVER_PORT || 3001);
+  await app.listen(process.env.PORT || 3001);
 }
 
 bootstrap();
